fb656a976f5497948d1a21922c71f5a7de89004f
aiden351 <122555616+aiden351@users.noreply.github.com>
2023-01-20 12:28:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,15 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(24)
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(0, 30))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.East)
+})
 for (let index = 0; index < 10; index++) {
     music.playMelody("G A F B G A D E ", 120)
 }
