e2451d41e2dab0ba6e88312f63d647d4989154d7
aiden351 <122555616+aiden351@users.noreply.github.com>
2023-01-20 11:44:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(24)
+})
+music.playMelody("- - - - - - - - ", 120)
+basic.forever(function () {
+	
+})
